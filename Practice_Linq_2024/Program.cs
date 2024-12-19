bc9c3303b528c0991dc2c19ed8a390e2ee3fd689
Artem Lytvynov <a.o.lytvynov@student.khai.edu>
2024-12-19 15:05:11 +0200

    Query3

--- Practice_Linq_2024/Program.cs
+++ Practice_Linq_2024/Program.cs
@@ -83,13 +83,16 @@ namespace Practice_Linq_2024
         {
             //Query 3: Вивести всі домашні матчі збірної Франції за 2021 рік, де вона зіграла у нічию.
 
-            var selectedGames = games;   // Корегуємо запит !!!
+            var selectedGames = games.Where(g => g.Home_team == "France" && g.Home_score == g.Away_score && g.Date.Year == 2021 && g.Country == "France");   // Корегуємо запит !!!
 
             // Перевірка
             Console.WriteLine("\n======================== QUERY 3 ========================");
 
             // див. приклад як має бути виведено:
-
+            foreach (FootballGame game in selectedGames)
+            {
+                Console.WriteLine($"{game.Date.ToString("dd.MM.yyyy")} {game.Home_team} - {game.Away_team}, Score: {game.Home_score} - {game.Away_score}, Country: {game.Country}");
+            }
 
         }
 
